1a81ee1bc8f6f2c8c1d88cece81c0a1d20603160
HamoHamoHamo <hamo222@naver.com>
2022-01-11 10:45:54 +0000

    jwt new setting

---
+++ my_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'attendance',
+        'USER': 'hamo',
+        'PASSWORD': '1404',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'pcqrou6*0yzfvs(30h)lafzgdt%o4u6-)0&n2e09m5_@$sv9(k'
